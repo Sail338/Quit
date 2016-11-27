6d6c0666d6379e3b99038effa65cc7c59b8bd0ed
Sail338 <dude.ex001@gmail.com>
2016-11-27 12:32:33 -0500

    started to add header files for treebuilder

--- tree_c/treebuilder.h
+++ tree_c/treebuilder.h
@@ -1,5 +1,9 @@
 #ifndef _treebuilder
 #define  _treebuilder
-GimMasterTree* InitGimMasterTree (char *root);
-
+extern GimMasterTree* InitGimMasterTree (char *root);
+extern TNode* GenerateTree(char *path);
+extern TNode* GenerateTNode(char *fpath);
+extern void WalkNode(TNode *node, int level);
+char *writeBlob(Blob *blolb);
+char * writeTree(TNode* root);
 #endif
